f74913fc57a2a31fd65188a2e0a46616d3cdccac
Myth197 <59457254+Myth197@users.noreply.github.com>
2025-06-13 12:39:48 +0100

    Automated Vendor Logic
    - Vendors light up when in logic, click them to mark them collected
    - Allow the user to click the vendors to mark it as junk
    - Remove old planet badges
    - Resize and re-orient Vendor and Planet trackers
    - Test location highlighting

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,5 @@
 --  Load configuration options up front
-ScriptHost:LoadScript("scripts/settings.lua")
-
+DEBUG = true
 Tracker:AddItems("items/common.json")
 
 if not (string.find(Tracker.ActiveVariantUID, "items_only")) then
@@ -16,3 +15,41 @@ end
 
 Tracker:AddLayouts("layouts/standard_broadcast.json")
 
+
+Planets = {
+	"Novalis",
+	"Kerwan",
+	"Aridia",
+	"Eudora",
+	"Blarg",
+	"Rilgar",
+	"Umbris",
+	"Batalia",
+	"Orxon",
+	"Gaspar",
+	"Poki",
+	"Hoven",
+	"Gemlik",
+	"Oltanis",
+	"Quartu",
+	"Kalebo",
+	"Fleet",
+	"Veldin"
+}
+
+for v, name in pairs(Planets) do
+	local infobot = Tracker:FindObjectForCode(name)
+	infobot.BadgeText = string.sub(name, 1, 3)
+	infobot:SetOverlayFontSize(12)
+	infobot:SetOverlayAlign("center")
+	if pcall(function()
+			Tracker:FindObjectForCode(name .. "_v").BadgeText = "Test"
+		end) then
+		local vendor = Tracker:FindObjectForCode(name .. "_v")
+		vendor.BadgeText = string.sub(name, 1, 3)
+		vendor:SetOverlayFontSize(12)
+		vendor:SetOverlayAlign("left")
+	end
+end
+
+ScriptHost:AddWatchForCode("Vendor in Logic", "*", Vendor_activate)
--- scripts/logic_common.lua
+++ scripts/logic_common.lua
@@ -27,3 +27,106 @@ function Rock_Explosion()
     return Tracker:ProviderCountForCode("RYNO")
   end
 end
+
+local Vendors = {
+  ['@Vendor/Novalis Vendor'] = "Novalis_v",
+  ['@Vendor/Kerwan Vendor'] = "Kerwan_v",
+  ['@Vendor/Eudora Vendor'] = "Eudora_v",
+  ['@Vendor/Blarg Vendor'] = "Blarg_v",
+  ['@Vendor/Rilgar Vendor'] = "Rilgar_v",
+  ['@Vendor/Batalia Vendor'] = "Batalia_v",
+  ['@Vendor/Orxon Vendor'] = "Orxon_v",
+  ['@Vendor/Gaspar Vendor'] = "Gaspar_v",
+  ['@Vendor/Poki Vendor'] = "Poki_v",
+  ['@Vendor/Hoven Vendor'] = "Hoven_v",
+  ['@Vendor/Oltanis Vendor'] = "Oltanis_v",
+}
+
+local Vendor_flags = {
+  ["Novalis_v"] = 0,
+  ["Kerwan_v"] = 0,
+  ["Eudora_v"] = 0,
+  ["Blarg_v"] = 0,
+  ["Rilgar_v"] = 0,
+  ["Batalia_v"] = 0,
+  ["Orxon_v"] = 0,
+  ["Gaspar_v"] = 0,
+  ["Poki_v"] = 0,
+  ["Hoven_v"] = 0,
+  ["Oltanis_v"] = 0,
+}
+
+local previous_code = ""
+local depth = 0
+
+function Vendor_activate(code)
+  depth = depth + 1
+  for location, item in pairs(Vendors) do
+    local trip = 0
+    if Tracker:FindObjectForCode(location).AccessibilityLevel == AccessibilityLevel.Normal and Vendor_flags[item] == 0 then
+      Vendor_flags[item] = 1
+      trip = 1
+    elseif Vendor_flags[item] == 0 and item == code then
+      if previous_code == code then
+        break
+      end
+      if previous_code .. "_v" == item then
+        break
+      end
+      Vendor_flags[item] = 2
+      trip = 1
+    elseif Tracker:FindObjectForCode(location).AccessibilityLevel < AccessibilityLevel.Normal and Vendor_flags[item] == 1 then
+      Vendor_flags[item] = 0
+      trip = 1
+    elseif Vendor_flags[item] == 1 and item == code then
+      if previous_code == code then
+        break
+      end
+      if previous_code .. "_v" == item then
+        break
+      end
+      Vendor_flags[item] = 2
+      trip = 1
+    elseif Tracker:FindObjectForCode(location).AccessibilityLevel < AccessibilityLevel.Normal and Vendor_flags[item] == 2 then
+      Vendor_flags[item] = 0
+      trip = 1
+    elseif Vendor_flags[item] == 2 and item == code then
+      if previous_code == code then
+        break
+      end
+      if previous_code .. "_v" == item then
+        break
+      end
+      Vendor_flags[item] = 1
+      trip = 1
+    end
+    if trip == 1 then
+      previous_code = code
+      Tracker:FindObjectForCode(item).CurrentStage = Vendor_flags[item]
+    else
+    end
+  end
+  depth = depth - 1
+  for k, v in pairs(Vendor_flags) do
+    if depth == 0 or v ~= Tracker:FindObjectForCode(k).CurrentStage then
+      Vendor_flags[k] = Tracker:FindObjectForCode(k).CurrentStage
+    end
+  end
+  if depth == 0 then
+    previous_code = ""
+  end
+end
+
+function Vendor_clear(code)
+  for location, item in pairs(Vendors) do
+    if code == item then
+      Tracker:FindObjectForCode(code).CurrentStage = 2
+    end
+  end
+end
+
+function Highlight(loc, priority)
+  Tracker:FindObjectForCode(loc).Highlight = priority
+  -- example
+  -- Tracker:FindObjectForCode("@Explore as Ratchet/Infobot").Highlight = Highlight.Priority
+end
